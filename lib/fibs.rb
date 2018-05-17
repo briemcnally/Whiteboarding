3b0f93ff880b0b36bb41d9be0ecde94a76d509d4
brie_mcnally <brienne.mcnally@gmail.com>
2018-05-17 12:35:25 -0700

    Add recursive sum

---
+++ lib/fibs.rb
@@ -0,0 +1,29 @@
+# Write a function, fibs(num) which returns the first n elements from
+# the fibonnacci sequence, given n.
+#
+# Solve it both iteratively and recursively.
+
+#iterative
+def fibs(num)
+  return [] if num == 0
+  return [0] if num == 1
+
+  fibs = [0, 1]
+  while fibs.length < num
+    fibs << fibs[-1] + fibs[-2]
+  end
+
+  fibs
+end
+
+#recursive
+
+def fibs(sum)
+  return [] if num == 0
+  return [0] if num == 1
+  return [0, 1] if num == 2
+
+  prev_fibs = fibs(num - 1)
+  prev_fibs << prev_fibs[-1] + prev_fibs[-2]
+  prev_fibs
+end
