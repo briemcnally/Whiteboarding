b122f4a6bedee5298aadb6d82a91c5db3df55e7d
brie_mcnally <brienne.mcnally@gmail.com>
2018-06-13 13:24:53 -0700

    Add password generator

---
+++ lib/password_generator.rb
@@ -0,0 +1,27 @@
+# 
+# Given four variables equal to a string of uppercase letters, lowercase
+# letters, numbers, and symbols, generate a random password based on a number provided.
+# Examples:
+#
+# generate_password(7) => 'Hj3@HB!'
+# generate_password(5) => '34567'
+# generate_password(2) => 'aC'
+# generate_password(4) => '!#$n'
+#
+# Additional constraints:
+# The user should be able to filter how he/she would like the password to
+# be randomly generated. For example, if the user only wanted a randomly
+# generated password consisting of numbers and uppercase letters,
+# generate_password(5) => '2Aa!5' would be invalid because it contains lowercase letters and symbols
+# generate_password(5) => '2A56B' would be a valid example
+#
+# Finally, when generating this password with filters, we need to
+# guarantee that there will be AT LEAST 1 character from each filter.
+# For example, if we filter the password to generate a password of just
+# uppercase letters and numbers, there's still a chance that the password
+# could generate a string of only numbers or only uppercase letters.
+#
+# generate_password(5) => '88324' would be invalid because it only contains numbers
+# generate_password(5) => 'GHJLK' would also be invalid because it only contains uppercase letters
+# generate_password(5) => '5A555' works because there's at least one number and one uppercase letter
+# generate_password(6) => 'BC7HJ' works because there's at least one number and one uppercase letter
