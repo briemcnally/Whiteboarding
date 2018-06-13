1f119573826ff9f854548f85efb46e5b846e9856
brie_mcnally <brienne.mcnally@gmail.com>
2018-06-13 13:59:40 -0700

    Add consecutive letters solution

--- lib/string_reduce.rb
+++ lib/string_reduce.rb
@@ -1,6 +1,16 @@
-# Given a string of characters change all occurrences of two or more 
+# Given a string of characters change all occurrences of two or more
 # consecutive 'e's with a single 'e'.
 #
 # Example:
 # Input: 'asdheeeeskaeeeleee'
 # Output: 'asdheskaele'
+
+def consecutive_letters(string)
+  output = ""
+
+  string.split("").each_with_index do |char, idx|
+    output += char if string[idx + 1] != char
+  end
+
+  output
+end
